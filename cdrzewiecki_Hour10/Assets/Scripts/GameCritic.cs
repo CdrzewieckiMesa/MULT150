a3d8f21e694ad2040c91cefaf8ec17fd514cee59
CdrzewieckiMesa <cdrzewiecki@student.sdccd.edu>
2024-11-10 13:30:27 -0800

    Hour 10 push

---
+++ cdrzewiecki_Hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,11 @@
+/* Improving the Game
+ * 1. How does a Chaos Ball player lose the game? 
+ *  Player can't really lose the game
+ * 2. What is the positional axis on which all the ball objects are constrained? 
+ *  At Y-position 2
+ * 3. True or false: The goals in Chaos Ball use the method OnTriggerEnter() to determine whether an object is the correct ball. 
+ *  True
+ * 4. Why are some basic features omitted from the game Chaos Ball?
+ *  To give the designer a chance to add them
+
+*/
