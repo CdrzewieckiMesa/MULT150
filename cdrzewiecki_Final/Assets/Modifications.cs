4b7bb6bc851a6ad7cc36c3341bd2a18a631138d4
CdrzewieckiMesa <cdrzewiecki@student.sdccd.edu>
2024-12-15 18:27:15 -0800

    Final Project push

---
+++ cdrzewiecki_Final/Assets/Modifications.cs
@@ -0,0 +1,14 @@
+/*
+ * 1.  Color of directional light was changed to a dark purple, and intensity was changed to 2.
+ * 2.  Fog was added to the game. Color: white  Density: .003
+ * 3.  Skybox added. Atmospheric thickness: 3.4 Dark purple sky tint
+ * 4.  Chaos prefab created. Chaos balls are now instances of the Chaos prefab. 
+ * 5.  Particle system: duration changed to 2 from 5.  start delay changed from 0 to 3.  start lifetime changed from 5 to 1.5.
+ * 6.  The blue, red, green, and orange balls are now prefabs.
+ * 7.  Sand texture was added to the blue, red, green, and orange balls.
+ * 8.  The blue, red, green, and orange goals changes: range: from 10 to 12.  Color: each color is now darker.  intensity: from 3 to 5.
+ * 9.  Created a new wall with 3 cubes, a cylinder.
+ * 10. Added the brick wall texture from hour 3 into the complex object. 
+ * 
+ * Other note: Loved this class!!! I hate the feeling that this class was really an easy A for me but just didn't work out as such though.
+ */
